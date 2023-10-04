fb6916efc3a053c79fb896f48c7275de51a625ac
gushchinda <gushchinda@autoins.ru>
2023-10-05 00:34:21 +0300

    repeat

--- tasks/Main.java
+++ tasks/Main.java
@@ -5,10 +5,11 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new LongestCommonPrefix__14();
+        Task task = new MedianOfTwoSortedArrays__4();
 
         Map params = new HashMap<>();
-        params.put("value",new String[]{"ab","a"});
+        params.put("list1",new int[]{1,2,3,4});
+        params.put("list2",new int[]{5,6,7,8,9,10,11,12});
 
         System.out.println(task.execute(params));
     }
--- tasks/MedianOfTwoSortedArrays__4.java
+++ tasks/MedianOfTwoSortedArrays__4.java
@@ -41,46 +41,43 @@ public class MedianOfTwoSortedArrays__4 implements Task {
             nums2 = temp;
         }
 
-        // Get the lengths of the two arrays
-        int len1 = nums1.length;
-        int len2 = nums2.length;
+        // Partition nums1 and nums2
+        int partition1 = nums1.length / 2;
+        int partition2 = (nums1.length + nums2.length + 1) / 2 - partition1;
 
-        // Set the range for binary search on nums1
-        int left = 0;
-        int right = len1;
-
-        while (left <= right) {
-            // Partition nums1 and nums2
-            int partition1 = (left + right) / 2;
-            int partition2 = (len1 + len2 + 1) / 2 - partition1;
-
+        while (partition1 <= nums1.length) {
             // Find the maximum elements on the left of the partition
             int maxLeft1 = partition1 > 0 ? nums1[partition1 - 1] : Integer.MIN_VALUE;
             int maxLeft2 = partition2 > 0 ? nums2[partition2 - 1] : Integer.MIN_VALUE;
             int maxLeft = Math.max(maxLeft1, maxLeft2);
 
             // Find the minimum elements on the right of the partition
-            int minRight1 = partition1 < len1 ? nums1[partition1] : Integer.MAX_VALUE;
-            int minRight2 = partition2 < len2 ? nums2[partition2] : Integer.MAX_VALUE;
+            int minRight1 = partition1 < nums1.length ? nums1[partition1] : Integer.MAX_VALUE;
+            int minRight2 = partition2 < nums2.length ? nums2[partition2] : Integer.MAX_VALUE;
             int minRight = Math.min(minRight1, minRight2);
 
             // Check if the partition is correct
             if (maxLeft <= minRight) {
                 // If the total length is even, return the average of the two middle elements
-                if ((len1 + len2) % 2 == 0) {
+                if ((nums1.length + nums2.length) % 2 == 0) {
                     return (maxLeft + minRight) / 2.0;
                 }
                 // If the total length is odd, return the middle element
                 else {
                     return maxLeft;
                 }
-            } else if (maxLeft1 > minRight2) {
-                right = partition1 - 1;
+            }
+            if (maxLeft1 > minRight2) {
+                partition1--;
+                partition2++;
             } else {
-                left = partition1 + 1;
+                partition1++;
+                partition2--;
             }
         }
 
         return 0.0;
+
+
     }
 }
