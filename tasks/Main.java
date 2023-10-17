39f5dcef567ac6df013b606cbd595a3fb1e348b3
gushchinda <gushchinda@autoins.ru>
2023-10-18 00:57:55 +0300

    26 task

--- tasks/Main.java
+++ tasks/Main.java
@@ -5,10 +5,10 @@ import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        Task task = new ValidParentheses__20();
+        Task task = new RemoveDuplicatesFromSortedArray__26();
 
         Map params = new HashMap<>();
-        params.put("value","[");
+        params.put("value",new int[]{0,0,1,1,1,2,2,3,3,4});
 
         System.out.println(task.execute(params));
     }
